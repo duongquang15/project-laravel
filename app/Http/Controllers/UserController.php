b070424ce94c50598be629b7e29f7175e211c27a
quangdv <quangdv2000@vnpt.vn>
2024-06-24 13:37:45 +0700

    clone code

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function show($id){
+        return User::findOrFail($id);
+    }
+
+    public function index(){
+        $users = User::
+        join('departments', 'users.department_id', '=', 'departments.id')
+        ->join('users_status', 'users.status_id', '=', 'users_status.id')
+        ->select('users.*', 'departments.name as departments', 'users_status.name as status')
+        ->get();
+        
+        return response()->json($users);
+    }
+}
